67ccc623739611baa23cdda4c34ac8996e77368e
Rowan Manning <info@rowanmanning.com>
2016-03-11 22:30:30 +0000

    Flesh out the command-line interface

--- bin/github-label-sync.js
+++ bin/github-label-sync.js
@@ -1,5 +1,56 @@
+#!/usr/bin/env node
 'use strict';
 
 const githubLabelSync = require('../lib/github-label-sync');
+const path = require('path');
+const pkg = require('../package.json');
+const program = require('commander');
 
-githubLabelSync();
+// Command-line configuration
+program
+	.version(pkg.version)
+	.usage('[options] <repository>')
+	.option(
+		'-a, --access-token <access-token>',
+		'a GitHub access token to make requests with',
+		process.env.GITHUB_ACCESS_TOKEN
+	)
+	.option(
+		'-l, --labels <path>',
+		'the path to look for the label configuration in. Default: ./.labels',
+		'.labels'
+	)
+	.parse(process.argv);
+
+// Make sure we have exactly one argument – the repo
+if (program.args.length !== 1) {
+	program.help();
+}
+
+// Resolve the label configuration path
+if (!/^[\/\~]/.test(program.labels)) {
+	program.labels = path.resolve(process.cwd(), program.labels);
+}
+
+// Load the labels
+let labels = [];
+try {
+	labels = require(program.labels);
+} catch (error) {
+	console.error(`No labels were found in ${program.labels}`);
+	process.exit(1);
+}
+
+// Pull together all the options
+const options = {
+	accessToken: program.accessToken,
+	labels: labels,
+	log: console,
+	repo: program.args[0]
+};
+
+// Sync the labels!
+githubLabelSync(options).catch((error) => {
+	console.error(error.stack || error.message);
+	process.exit(1);
+});
